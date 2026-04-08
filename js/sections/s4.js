564d3d2d83b4a6b82d37d17045bbadafa4925fcc
theodiez <theodiez@users.noreply.github.com>
2026-04-08 14:36:14 +0200

    auto-update 2026-04-08 14:36:14

--- js/sections/s4.js
+++ js/sections/s4.js
@@ -1,4 +1,13 @@
 // js/sections/s4.js
 import { renderWeekTracker, initTracker } from '../tracker.js';
-export function renderS4(meta) { return renderWeekTracker('s4', meta); }
-export function initS4() { initTracker('s4'); }
+import { renderWeekSummaryButton, initWeekSummaryButton, checkAutoShowSummary } from './weeksummary.js';
+
+export function renderS4(meta) {
+    return renderWeekTracker('s4', meta) + renderWeekSummaryButton('s4');
+}
+
+export function initS4() {
+    initTracker('s4');
+    initWeekSummaryButton('s4', 's4');
+    checkAutoShowSummary('s4');
+}
--- js/sections/s5.js
+++ js/sections/s5.js
@@ -1,4 +1,13 @@
 // js/sections/s5.js
 import { renderWeekTracker, initTracker } from '../tracker.js';
-export function renderS5(meta) { return renderWeekTracker('s5', meta); }
-export function initS5() { initTracker('s5'); }
+import { renderWeekSummaryButton, initWeekSummaryButton, checkAutoShowSummary } from './weeksummary.js';
+
+export function renderS5(meta) {
+    return renderWeekTracker('s5', meta) + renderWeekSummaryButton('s5');
+}
+
+export function initS5() {
+    initTracker('s5');
+    initWeekSummaryButton('s5', 's5');
+    checkAutoShowSummary('s5');
+}
