44703861afb68cdb86842de1731d10375f0474a7
theodiez <theodiez@users.noreply.github.com>
2026-04-15 19:59:31 +0200

    auto-update 2026-04-15 19:59:31

---
+++ .wrangler/tmp/pages-rSkfiZ/functionsRoutes-0.1482545403426404.mjs
@@ -0,0 +1,27 @@
+import { onRequestGet as __api_push_subscribe_ts_onRequestGet } from "/Users/theodiez/Documents/Programme CANDITO/functions/api/push-subscribe.ts"
+import { onRequestOptions as __api_push_subscribe_ts_onRequestOptions } from "/Users/theodiez/Documents/Programme CANDITO/functions/api/push-subscribe.ts"
+import { onRequestPost as __api_push_subscribe_ts_onRequestPost } from "/Users/theodiez/Documents/Programme CANDITO/functions/api/push-subscribe.ts"
+
+export const routes = [
+    {
+      routePath: "/api/push-subscribe",
+      mountPath: "/api",
+      method: "GET",
+      middlewares: [],
+      modules: [__api_push_subscribe_ts_onRequestGet],
+    },
+  {
+      routePath: "/api/push-subscribe",
+      mountPath: "/api",
+      method: "OPTIONS",
+      middlewares: [],
+      modules: [__api_push_subscribe_ts_onRequestOptions],
+    },
+  {
+      routePath: "/api/push-subscribe",
+      mountPath: "/api",
+      method: "POST",
+      middlewares: [],
+      modules: [__api_push_subscribe_ts_onRequestPost],
+    },
+  ]
